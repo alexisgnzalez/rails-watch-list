d777212ca38e2f90907a55def8cab235e7b039f8
Alexis Gonzalez <1991.alexis@gmail.com>
2021-10-22 10:59:25 -0400

    Landing page view done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
   validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie, scope: :list
-  # validates :movie, :list, uniqueness: { [:movie, :list] }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
 end
